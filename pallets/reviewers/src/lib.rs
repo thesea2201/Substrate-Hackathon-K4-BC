98103ca627e25b1e2ffb6fb31944521a04947776
LaiVoJM <84391289+LaiVoJM@users.noreply.github.com>
2022-12-31 16:34:24 +0700

    update-reviewer

--- pallets/reviewers/src/lib.rs
+++ pallets/reviewers/src/lib.rs
@@ -20,11 +20,8 @@ use frame_system::pallet_prelude::*;
 use codec::{Encode, MaxEncodedLen, Decode};
 use frame_support::pallet_prelude::BoundedVec;
 use scale_info::TypeInfo;
-// use crate::Config;
-// use frame_support::Blake2_128Concat;
 use sp_runtime::traits::Hash;
 use sp_runtime::ArithmeticError;
-// use sp_std::vec::Vec;
 use frame_support::traits::Randomness;
 use frame_support::dispatch::Vec;
 
@@ -39,6 +36,8 @@ pub mod pallet {
 		pub id: T::Hash,
 		pub name: Name,
 		pub star: Option<u32>, 
+		pub owner: T::AccountId,
+
 	}
 
 
@@ -55,10 +54,9 @@ pub mod pallet {
 	#[pallet::without_storage_info]
 	pub struct Pallet<T>(_);
 
-	/// Configure the pallet by specifying the parameters and types on which it depends.
+	
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
-		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 		type IdRandom: Randomness<Self::Hash, Self::BlockNumber>;
 
@@ -80,19 +78,17 @@ pub mod pallet {
 	pub type ReviewersAccountOwned<T:Config> = StorageMap<_, Blake2_128Concat, T::AccountId, BoundedVec<T::Hash, T::MaxAccount> , ValueQuery>;
 
 
-	// Pallets use events to inform users when important changes are made.
-	// https://docs.substrate.io/main-docs/build/events-errors/
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		CreateReviewer {reviewer: T::Hash, who: T::AccountId}
+		CreateReviewer {reviewer: T::Hash, who: T::AccountId},
+		UpdateReviewer {reviewer: T::Hash}
 	}
 
-	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
 		ReviewerNotFound,
-		NotOwnedAccount,
+		NotOwnedAccountReviewer,
 		DuplicateReviewer,
 		TooManyOwned,
 	}
@@ -116,6 +112,7 @@ pub mod pallet {
 				id: id.clone(),
 				name: name,
 				star: star,
+				owner: who.clone()
 			};
 
 			ensure!(!Reviewers::<T>::contains_key(&reviewer.id), Error::<T>::DuplicateReviewer);
@@ -129,8 +126,6 @@ pub mod pallet {
 				list_reviewer.try_push(id.clone())
 			}).map_err(|_| <Error<T>>::TooManyOwned)?;
 
-			// ReviewersAccountOwned::<T>::append(&who, reviewer.id.clone());
-
 			Reviewers::<T>::insert(reviewer.id.clone(), reviewer);
 
 			Self::deposit_event(Event::CreateReviewer { reviewer: id.clone(), who: who.clone() });
@@ -138,25 +133,28 @@ pub mod pallet {
 			Ok(())
 		}
 
-		// An example dispatchable that may throw a custom error.
-		// #[pallet::call_index(1)]
-		// #[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1).ref_time())]
-		// pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-		// 	let _who = ensure_signed(origin)?;
+		#[pallet::call_index(1)]
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1).ref_time())]
+		pub fn update_reviewer(origin: OriginFor<T>, id: T::Hash, name: Name, star: Option<u32>) -> DispatchResult {
+			let who = ensure_signed(origin)?;
 
-		// 	// Read a value from storage.
-		// 	match <Something<T>>::get() {
-		// 		// Return an error if the value has not been set.
-		// 		None => return Err(Error::<T>::NoneValue.into()),
-		// 		Some(old) => {
-		// 			// Increment the value read from storage; will error in the event of overflow.
-		// 			let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-		// 			// Update the value in storage with the incremented result.
-		// 			<Something<T>>::put(new);
-		// 			Ok(())
-		// 		},
-		// 	}
-		// }
+			let reviewer = Reviewers::<T>::get(id.clone()).ok_or(Error::<T>::ReviewerNotFound)?;
+
+			ensure!(reviewer.owner.clone() == who, Error::<T>::NotOwnedAccountReviewer);
+			
+			Reviewers::<T>::try_mutate(&id, |change| {
+				if let Some(reviewer) = change {
+					reviewer.name = name;
+					reviewer.star = star;
+					return Ok(());
+				}
+				Err(())
+			}).map_err(|_| Error::<T>::ReviewerNotFound)?;
+			
+			Self::deposit_event(Event::UpdateReviewer { reviewer: id.clone()});
+			
+			Ok(())
+		}
 	}
 
 	
